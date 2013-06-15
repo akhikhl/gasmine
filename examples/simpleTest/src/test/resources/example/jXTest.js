160f32fd68eac6e06bbd41f9abbf5f6a9ea04afd
Andrey Hihlovskiy <akhikhl@gmail.com>
2013-06-16 01:27:10 +0200

    introduced jXTest

---
+++ examples/simpleTest/src/test/resources/example/jXTest.js
@@ -0,0 +1,124 @@
+describe("jX monad", function() {
+  it("should wrap/unwrap null", function() {
+    expect(jX(null).get()).toBeNull();
+  });
+  it("should wrap/unwrap number", function() {
+    var x = 123;
+    expect(jX(x).get()).toBe(x);
+  });
+  it("should wrap/unwrap string", function() {
+    var x = "hello";
+    expect(jX(x).get()).toBe(x);
+  });
+  it("should wrap/unwrap array", function() {
+    var x = [ 123, "hello" ];
+    expect(jX(x).get()).toBe(x);
+  });
+  it("should wrap/unwrap object", function() {
+    var x = { color: "red", name: "apple" };
+    expect(jX(x).get()).toBe(x);
+  });
+  it("should double wrap", function() {
+    var x = { color: "red", name: "apple" };
+    var w = jX(x);
+    expect(jX(w)).toBe(w);
+  });
+});
+
+describe("jX reverse", function() {
+
+  beforeEach(function() {
+    if(!jX.fn.reverse)
+      jX.fn.reverse = function() {
+        return this.transform({
+          transformArray: function(arr) {
+            return arr.reverse();
+          },
+          transformString: function(s) {
+            var o = '';
+            for (var i = s.length - 1; i >= 0; i--)
+              o += s[i];
+            return o;        
+          }
+        });
+      };
+  });
+  
+  it("should reverse an array", function() {
+    expect(jX(["a", "b", "c"]).reverse().get()).toEqual(["c", "b", "a"]);
+  });
+  it("should reverse a string", function() {
+    expect(jX("abc").reverse().get()).toEqual("cba");
+  });
+  it("should reverse arrays within object", function() {
+    expect(jX({ one: ["a", "b"], two: ["c", "d"] }).reverse().get()).toEqual({ one: ["b", "a"], two: ["d", "c"] });
+  });
+  it("should reverse strings and arrays within object", function() {
+    expect(jX({ one: ["abc", "def"] }).reverse().get()).toEqual({ one: ["fed", "cba"] });
+  });
+});
+
+describe("jX toJS", function() {
+  
+  it("should retain intact JS-string", function() {
+    expect(jX("abc").toJS().get()).toEqual("abc");
+  });
+  it("should convert Java-string to JS-string", function() {
+    expect(jX(new java.lang.String("abc")).toJS().get()).toEqual("abc");
+  });
+  it("should retain intact JS-number", function() {
+    expect(jX(123).toJS().get()).toEqual(123);
+  });
+  it("should convert Java-integer to JS-number", function() {
+    expect(jX(new java.lang.Integer(123)).toJS().get()).toEqual(123);
+  });
+  it("should convert Java-double to JS-number", function() {
+    expect(jX(new java.lang.Double(123.5)).toJS().get()).toEqual(123.5);
+  });
+  it("should convert Java-boolean-true to JS-boolean-true", function() {
+    expect(jX(java.lang.Boolean.TRUE).toJS().get()).toBe(true);
+  });
+  it("should convert Java-boolean-false to JS-boolean-false", function() {
+    expect(jX(java.lang.Boolean.FALSE).toJS().get()).toBe(false);
+  });
+  it("should retain intact JS-boolean-true", function() {
+    expect(jX(true).toJS().get()).toEqual(true);
+  });
+  it("should retain intact JS-boolean-false", function() {
+    expect(jX(false).toJS().get()).toEqual(false);
+  });
+  it("should convert Java-char to JS-string", function() {
+    expect(jX(new java.lang.Character(new java.lang.String("a").charAt(0))).toJS().get()).toEqual("a");
+  });
+  it("should retain intact JS-array", function() {
+    expect(jX(["a", "b"]).toJS().get()).toEqual(["a", "b"]);
+  });
+  it("should convert Java-array to JS-array", function() {
+    expect(jX(toJavaArray(["aaa", "bbb"])).toJS().get()).toEqual(["aaa", "bbb"]);
+  });
+  it("should convert Java-Collection to JS-array", function() {
+    let list = new java.util.LinkedList();
+    list.add("aaa");
+    list.add("bbb");
+    expect(jX(list).toJS().get()).toEqual(["aaa", "bbb"]);
+  });
+  it("should retain intact JS-object", function() {
+    expect(jX({ type: "apple", color: "red" }).toJS().get()).toEqual({ type: "apple", color: "red" });
+  });
+  it("should convert Java-Map to JS-object", function() {
+    let map = new java.util.LinkedHashMap();
+    map.put("type", "apple");
+    map.put("color", "red");
+    expect(jX(map).toJS().get()).toEqual({ type: "apple", color: "red" });
+  });
+  it("should convert nested Java structures to JS-objects", function() {
+    let map = new java.util.LinkedHashMap();
+    map.put("type", "apple");
+    map.put("color", "red");
+    let list = new java.util.LinkedList();
+    list.add("big");
+    list.add("small");
+    map.put("sizes", list);
+    expect(jX(map).toJS().get()).toEqual({ type: "apple", color: "red", sizes: ["big", "small"] });
+  });
+});
