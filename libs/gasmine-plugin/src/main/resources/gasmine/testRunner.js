fa30da36f33b1095d0e9b2f5e0f6638300c5fb40
Andrey Hihlovskiy <akhikhl@gmail.com>
2013-06-16 01:26:38 +0200

    fixed service initialization in testRunner

--- libs/gasmine-plugin/src/main/resources/gasmine/testRunner.js
+++ libs/gasmine-plugin/src/main/resources/gasmine/testRunner.js
@@ -11,51 +11,39 @@ load("gasmine/consoleReporter.js");
   let FileFilter = java.io.FileFilter;
   let FilenameUtils = org.apache.commons.io.FilenameUtils;
   
-  return function(args) {
+  function loadTestScripts_(folder) {
+    let subFolders = folder.listFiles(FileFilter({ accept: function(f) { return f.isDirectory(); } }));
+    if(subFolders != null)
+      for each(let subFolder in subFolders)
+        loadTestScripts_(subFolder);
+    let files = folder.listFiles(FileFilter({ accept: function(f) { 
+      if(!f.isFile())
+        return false;
+      let fname = f.getName();
+      let fbaseName = FilenameUtils.getBaseName(fname);
+      return fname.endsWith(".js") && (fbaseName.startsWith("Test") || fbaseName.endsWith("Test"));
+    } }));
+    if(files != null)
+      for each(let f in files) {
+        logger.trace("Loading test script: {}", f.getName());
+        load(f);
+      }
+  }
   
-    let scriptDirectory = args[0];
-  
-    function loadTestScripts_(folder) {
-      let subFolders = folder.listFiles(FileFilter({ accept: function(f) { return f.isDirectory(); } }));
-      if(subFolders != null)
-        for each(let subFolder in subFolders)
-          loadTestScripts_(subFolder);
-      let files = folder.listFiles(FileFilter({ accept: function(f) { 
-        if(!f.isFile())
-          return false;
-        let fname = f.getName();
-        let fbaseName = FilenameUtils.getBaseName(fname);
-        return fname.endsWith(".js") && (fbaseName.startsWith("Test") || fbaseName.endsWith("Test"));
-      } }));
-      if(files != null)
-        for each(let f in files) {
-          logger.trace("Loading test script: {}", f.getName());
-          load(f);
-        }
-    }
-    
-    let env = global.jasmine.getEnv();
-    
-    let reporter = new global.ConsoleReporter(logger);  
-    env.addReporter(reporter);
-    
-    let globalLogger = global.logger;
-    try {
-      global.logger = logger; 
-        
-      loadTestScripts_(scriptDirectory);
-      
+  return {
+    beforeMain: function(args) {
+      loadTestScripts_(args[0]);
+    },
+    main: function(args) {
+      let env = global.jasmine.getEnv();
+      let reporter = new global.ConsoleReporter(logger);  
+      env.addReporter(reporter);
       env.execute();
-      
       if(reporter.failCount() == 0)
         logger.warn("RESULTS: {} passed.", Number(reporter.passedCount()).toFixed(0));
       else
         logger.error("RESULTS: {} passed, {} FAILED.", Number(reporter.passedCount()).toFixed(0), Number(reporter.failCount()).toFixed(0));
-      
-    } finally {
-      global.logger = globalLogger;
-    }    
-    
-    return reporter.failCount();
+      return reporter.failCount();
+    }
   };
 })(this);
