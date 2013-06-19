6a8741ec47fbfce74dc8c1c6824e28de7ac030a7
Andrey Khikhlovskiy <ahi@startext.de>
2013-06-19 09:31:13 +0200

    accomodated jXTest.js to latest changes in jXtoJSON

--- examples/simpleTest/src/test/resources/example/jXTest.js
+++ examples/simpleTest/src/test/resources/example/jXTest.js
@@ -145,77 +145,77 @@ describe("jX toJS", function() {
 describe("jX toJSONString", function() {
 
   it("should convert null to JSON", function() {
-    expect(jX(null).toJSONString().get()).toEqual("null");
+    expect(jX(null).toJSONString().get()).toEqual('null');
   });
   it("should convert undefined to JSON", function() {
-    expect(jX(undefined).toJSONString().get()).toEqual("undefined");
+    expect(jX(undefined).toJSONString().get()).toEqual('undefined');
   });
   it("should convert JS-string to JSON", function() {
-    expect(jX("ab\"c").toJSONString().get()).toEqual("\"ab\\\"c\"");
+    expect(jX('ab"c').toJSONString().get()).toEqual('"ab\\"c"');
   });
   it("should convert Java-string to JSON", function() {
-    expect(jX(new java.lang.String("ab\"c")).toJSONString().get()).toEqual("\"ab\\\"c\"");
+    expect(jX(new java.lang.String('ab"c')).toJSONString().get()).toEqual('"ab\\"c"');
   });
   it("should convert JS-integer to JSON", function() {
-    expect(jX(123).toJSONString().get()).toEqual("123");
+    expect(jX(123).toJSONString().get()).toEqual('123');
   });
   it("should convert JS-number to JSON", function() {
-    expect(jX(123.5).toJSONString().get()).toEqual("123.5");
+    expect(jX(123.5).toJSONString().get()).toEqual('123.5');
   });
   it("should convert Java-integer to JSON", function() {
-    expect(jX(new java.lang.Integer(123)).toJSONString().get()).toEqual("123");
+    expect(jX(new java.lang.Integer(123)).toJSONString().get()).toEqual('123');
   });
   it("should convert Java-double to JSON", function() {
-    expect(jX(new java.lang.Double(123.5)).toJSONString().get()).toEqual("123.5");
+    expect(jX(new java.lang.Double(123.5)).toJSONString().get()).toEqual('123.5');
   });
   it("should convert JS-boolean-true to JSON", function() {
-    expect(jX(true).toJSONString().get()).toEqual("true");
+    expect(jX(true).toJSONString().get()).toEqual('true');
   });
   it("should convert JS-boolean-false to JSON", function() {
-    expect(jX(false).toJSONString().get()).toEqual("false");
+    expect(jX(false).toJSONString().get()).toEqual('false');
   });
   it("should convert Java-boolean-true to JSON", function() {
-    expect(jX(java.lang.Boolean.TRUE).toJSONString().get()).toEqual("true");
+    expect(jX(java.lang.Boolean.TRUE).toJSONString().get()).toEqual('true');
   });
   it("should convert Java-boolean-false to JSON", function() {
-    expect(jX(java.lang.Boolean.FALSE).toJSONString().get()).toEqual("false");
+    expect(jX(java.lang.Boolean.FALSE).toJSONString().get()).toEqual('false');
   });
   it("should convert Java-char to JSON", function() {
-    expect(jX(new java.lang.Character(new java.lang.String("a").charAt(0))).toJSONString().get()).toEqual("\"a\"");
+    expect(jX(new java.lang.Character(new java.lang.String("a").charAt(0))).toJSONString().get()).toEqual('"a"');
   });
   it("should convert empty JS-array to JSON", function() {
-    expect(jX([]).toJSONString().get()).toEqual("[]");
+    expect(jX([]).toJSONString().get()).toEqual('[]');
   });
   it("should convert JS-array to JSON", function() {
-    expect(jX(["a", "b", 123]).toJSONString().get()).toEqual("[ \"a\", \"b\", 123 ]");
+    expect(jX(["a", "b", 123]).toJSONString().get()).toEqual('[ "a", "b", 123 ]');
   });
   it("should convert Java-array to JSON", function() {
-    expect(jX(toJavaArray(["aaa", "bbb"])).toJSONString().get()).toEqual("[ \"aaa\", \"bbb\" ]");
+    expect(jX(toJavaArray(["aaa", "bbb"])).toJSONString().get()).toEqual('[ "aaa", "bbb" ]');
   });
   it("should convert Java-Collection to JSON", function() {
     let list = new java.util.LinkedList();
     list.add("aaa");
     list.add("bbb");
-    expect(jX(list).toJSONString().get()).toEqual("[ \"aaa\", \"bbb\" ]");
+    expect(jX(list).toJSONString().get()).toEqual('[ "aaa", "bbb" ]');
   });
   it("should convert JS-object to JSON", function() {
-    expect(jX({ type: "apple", color: "red" }).toJSONString().get()).toEqual("{ type: \"apple\", color: \"red\" }");
+    expect(jX({ type: "apple", color: "red" }).toJSONString().get()).toEqual('{ "type": "apple", "color": "red" }');
   });
   it("should convert empty JS-object to JSON", function() {
-    expect(jX({}).toJSONString().get()).toEqual("{}");
+    expect(jX({}).toJSONString().get()).toEqual('{}');
   });
   it("should convert JS-object with non-ident-keys to JSON", function() {
-    expect(jX({ type: "apple", "color-value": "red" }).toJSONString().get()).toEqual("{ type: \"apple\", \"color-value\": \"red\" }");
+    expect(jX({ type: "apple", "color-value": "red" }).toJSONString().get()).toEqual('{ "type": "apple", "color-value": "red" }');
   });
   it("should convert complex JS-object to JSON", function() {
     expect(jX({ type: "apple", color: "red", sizes: [ "big", "small" ] }).toJSONString().get())
-      .toEqual("{ type: \"apple\", color: \"red\", sizes: [ \"big\", \"small\" ] }");
+      .toEqual('{ "type": "apple", "color": "red", "sizes": [ "big", "small" ] }');
   });
   it("should convert Java-Map to JSON", function() {
     let map = new java.util.LinkedHashMap();
     map.put("type", "apple");
     map.put("color", "red");
-    expect(jX(map).toJSONString().get()).toEqual("{ type: \"apple\", color: \"red\" }");
+    expect(jX(map).toJSONString().get()).toEqual('{ "type": "apple", "color": "red" }');
   });
   it("should convert nested Java structures to JSON", function() {
     let map = new java.util.LinkedHashMap();
@@ -225,19 +225,19 @@ describe("jX toJSONString", function() {
     list.add("big");
     list.add(new java.lang.String("small"));
     map.put("sizes", list);
-    expect(jX(map).toJSONString().get()).toEqual("{ type: \"apple\", color: \"red\", sizes: [ \"big\", \"small\" ] }");
+    expect(jX(map).toJSONString().get()).toEqual('{ "type": "apple", "color": "red", "sizes": [ "big", "small" ] }');
   });
   it("should convert nulls within JS-object to JSON", function() {
-    expect(jX({ type: "apple", color: null }).toJSONString().get()).toEqual("{ type: \"apple\", color: null }");
+    expect(jX({ type: "apple", color: null }).toJSONString().get()).toEqual('{ "type": "apple", "color": null }');
   });
   it("should convert undefined within JS-object to JSON", function() {
-    expect(jX({ type: "apple", color: undefined }).toJSONString().get()).toEqual("{ type: \"apple\" }");
+    expect(jX({ type: "apple", color: undefined }).toJSONString().get()).toEqual('{ "type": "apple" }');
   });
   it("should convert complex JS-object with exclusions to JSON", function() {
     expect(jX({ type: "apple", color: "red", sizes: [ "big", "small" ] }).toJSONString({ excludeKeys: [ "sizes" ] }).get())
-      .toEqual("{ type: \"apple\", color: \"red\" }");
+      .toEqual('{ "type": "apple", "color": "red" }');
   });
   it("should convert function to JSON", function() {
-    expect(jX(function(x, y) { return x + y; }).toJSONString().get()).toEqual("\nfunction (x, y) {\n    return x + y;\n}\n");
+    expect(jX(function(x, y) { return x + y; }).toJSONString().get()).toEqual('\nfunction (x, y) {\n    return x + y;\n}\n');
   });
 });
