855bf77feb53a27b78c1f185189d182b787437d0
Andrey Hihlovskiy <akhikhl@gmail.com>
2015-03-02 10:42:37 +0100

    fixed classloader in GasminePlugin

--- buildSrc/gasmine-plugin/src/main/groovy/gasmine/GasminePlugin.groovy
+++ buildSrc/gasmine-plugin/src/main/groovy/gasmine/GasminePlugin.groovy
@@ -9,28 +9,32 @@ class GasminePlugin implements Plugin<Project> {
   void apply(final Project project) {
 
     project.dependencies {
-      testCompile "ch.qos.logback:logback-classic:1.0.6"
-      testCompile "junit:junit:4.10"
+      testCompile 'ch.qos.logback:logback-classic:1.1.2'
+      testCompile 'junit:junit:4.10'
     }
 
-    project.task("testScripts") { task ->
+    project.task('testScripts') { task ->
       doLast {
-        File scriptDirectory = project.file("src/test/resources")
+        File scriptDirectory = project.file('src/test/resources')
 
         def urls = [
-          new File(project.buildDir, "classes/main").toURI().toURL(),
-          new File(project.buildDir, "classes/test").toURI().toURL(),
-          new File(project.buildDir, "resources/main").toURI().toURL(),
-          new File(project.buildDir, "resources/test").toURI().toURL()
+          new File(project.buildDir, 'classes/main').toURI().toURL(),
+          new File(project.buildDir, 'classes/test').toURI().toURL(),
+          new File(project.buildDir, 'resources/main').toURI().toURL(),
+          new File(project.buildDir, 'resources/test').toURI().toURL()
         ]
-        urls += project.configurations["runtime"].collect { dep -> dep.toURI().toURL() }
+
+        urls += project.configurations.testRuntime.findAll { f ->
+          !f.name.contains('groovy-all')
+        }.collect { dep -> dep.toURI().toURL() }
+        
         URLClassLoader classLoader = new URLClassLoader(urls as URL[], GasminePlugin.class.classLoader)
 
-        def result = gino.Runner.run("gasmine/testRunner.js", [scriptDirectory ] as Object[], classLoader, logger, true, new File(project.buildDir, "resources/test").absolutePath)
+        def result = gino.Runner.run('gasmine/testRunner.js', [scriptDirectory ] as Object[], classLoader, logger, true, new File(project.buildDir, 'resources/test').absolutePath)
 
         if(result != 0)
         // assuming that test reporting tool already explained the reason of failure.
-        throw new GradleException("Build interrupted because of test errors")
+        throw new GradleException('Build interrupted because of test errors')
       }
 
       task.dependsOn project.tasks.test
